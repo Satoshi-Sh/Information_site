f421102c7565581a86e9ab0f4e9f600973dc7597
Satoshi-Sh <satoss1108@gmail.com>
2022-11-02 14:19:21 -0500

    Complete

---
+++ index.js
@@ -0,0 +1,34 @@
+var http = require('http');
+
+fs = require('fs');
+
+
+try {
+http.createServer(function(req,res){
+    let file;
+
+    if (req.url=='/'){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        file='./index.html'
+    }
+    else if (req.url=='/about'){
+        res.writeHead(200,{'Content-Type':'text/html'})
+        file='./about.html'
+    }
+    else if (req.url=='/contact-me') {
+        res.writeHead(200,{'Content-Type':'text/html'})
+        file = './contact-me.html'
+    }
+    else {
+        res.writeHead(404,{'Content-Type':'text/html'})
+        file ='./404.html'
+    }
+   
+    const data = fs.readFileSync(file,'utf8')
+    res.end(data)
+}).listen(3000)}
+catch (err) {
+  console.error(err);
+};
+
+
